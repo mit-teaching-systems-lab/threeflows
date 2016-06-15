a33ad155c3a81fb4047fcae128e4716ee495f82a
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-06-15 17:01:08 -0400

    Add minimal express server to save game data for Heroku deploy

--- README.md
+++ README.md
@@ -1,13 +1,38 @@
 # threeflows
-Design prototypes for flowing through a challenge.
+A barebones Express server and React webapp for doing design sketches of flows through a challenge and pieces of the challenge experience.
 
-## Running locally
-Start a watch process to build the project:
+## Running Locally
+#### Server
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.  To run remote Postgres commands you'll also need to [install Postgres locally](https://devcenter.heroku.com/articles/heroku-postgresql).
+
+```
+$ npm install
+$ npm start
+```
+
+#### UI build process
+In another terminal, start a watch process to build the project:
 ```
 $ npm run watch
 ```
 
-In another terminal, start a server:
+## Deploying to Heroku
+
 ```
-$ npm start
+$ heroku create
+$ git push heroku master
+$ heroku open
 ```
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Documentation
+
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
+- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
---
+++ server/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var url = require('url');
+var _ = require('lodash')
+var fs = require('fs');
+var bodyParser = require('body-parser')
+
+// create and configure server
+var app = express();
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({    // to support URL-encoded bodies
+  extended: true
+}));
+
+
+
+// serve static HTML
+function readFile(filename) {
+  return function(request, response) {
+    const string = fs.readFileSync(__dirname + '/../ui/build/' + filename);
+    response.end(string);
+  }
+}
+const html = 'index.html';
+const bundle = 'bundle.js';
+app.get('/', readFile(html));
+app.get('/bundle.js', readFile(bundle));
+app.get('/challenge/:id', readFile(html));
+app.get('/message_popup', readFile(html));
+
+
+
+// api routes
+// helper for db connection pooling
+var pg = require('pg');
+function queryDatabase(text, values, cb) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query(text, values, function(err, result) {
+      done();
+      cb(err, result);
+    });
+  });
+};
+
+app.get('/server/message_popup', function(request, response) {
+  // const values = [JSON.stringify(request.body)];
+  var urlParts = url.parse(request.url, true);
+  var query = urlParts.query;
+  const values = [JSON.stringify(query)];
+  queryDatabase('INSERT INTO message_popup_responses(json) VALUES ($1)', values, function(err, result) {
+    console.log(JSON.stringify(result));
+    response.json({result});  
+  });
+});
+
+app.get('/server/dump', function(request, response) {
+  queryDatabase('SELECT * FROM message_popup_responses', [], function(err, result) {
+    console.log(JSON.stringify(result));
+    response.json({result});  
+  });
+});
+
+
+// start server
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
