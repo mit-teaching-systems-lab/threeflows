fe2b3ffac2126e94669e495c0b850499a379d5db
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-06-16 10:00:54 -0400

    Revert "Validating the Travis build fails when there's a flow error"

--- ui/src/message_popup/message_popup_page.jsx
+++ ui/src/message_popup/message_popup_page.jsx
@@ -34,9 +34,9 @@ function logLocalStorage(record) {
 function logDatabase(record) {
   request
     .post(Routes.evidencePath({
-      afdspp: 'threeflows',
-      tyfdspe: 'message_popup',
-      verfdsfsion: 2
+      app: 'threeflows',
+      type: 'message_popup',
+      version: 2
     }))
     .set('Content-Type', 'application/json')
     .send(record)
