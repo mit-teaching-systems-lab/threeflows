7319a98958209023886f5c64f467b35641fe3396
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-06-23 16:29:41 -0400

    In progress

--- ui/src/message_popup/evaluation_page.jsx
+++ ui/src/message_popup/evaluation_page.jsx
@@ -1,5 +1,4 @@
 // @flow
-import _ from 'lodash';
 import React from 'react';
 import VelocityTransitionGroup from "velocity-react/velocity-transition-group";
 import 'velocity-animate/velocity.ui';
