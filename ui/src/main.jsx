056999f7459aa6ab82fd5de3e64e727f7faca13f
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-07-12 10:09:39 -0400

    Add rollbar to report JS errors

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,7 +2,26 @@
 import App from './app.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Rollbar from 'rollbar-browser';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  startRollbar();
   ReactDOM.render(<App history={true}/>, document.getElementById('app-main'));
 });
+
+
+function startRollbar() {
+  const {hostname, protocol, port} = window.location;
+  const portString = port === '' ? '' : `:${port}`;
+  const environment = `${protocol}//${hostname}${portString}`;
+
+  window.rollbar = Rollbar.init({
+    accessToken: "de42c0395e924afba679510bd16908a6",
+    captureUncaught: true,
+    captureUnhandledRejections: false,
+    payload: {
+      environment: environment
+    }
+  });
+}
