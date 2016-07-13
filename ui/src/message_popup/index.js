05cec0dbd8c9a9aee5472377969c4ce5e790259d
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-07-13 11:49:06 -0400

    Add two minimal views for evaluations of candidate competencies, with refactors
    Factoring out problem context and initial evaluation/id endpoint
    
    Add log into evaluation page, factor out UI components
    
    Restore livereload
    
    Done stitching together candidate and evaluation page
    
    Cleanup candidate page styles
    
    Remove noisy flow
    
    Refactoring candidate_page
    
    Fixup flow errors

--- ui/src/message_popup/index.js
+++ ui/src/message_popup/index.js
@@ -2,5 +2,6 @@
 import ExperiencePage from './experience_page.jsx';
 import ExplorationPage from './exploration_page.jsx';
 import ScoringPage from './scoring_page.jsx';
+import EvaluationViewerPage from './evaluation_viewer_page.jsx';
 
-export {ExperiencePage, ExplorationPage, ScoringPage};
+export {ExperiencePage, ExplorationPage, ScoringPage, EvaluationViewerPage};
