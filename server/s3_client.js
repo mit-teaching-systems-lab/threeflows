c74d6cc74e697493f1c71552f9ef143ac6e5f96c
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-08-10 10:29:25 -0400

    Read AWS credentials for audio as single env var

--- server/s3_client.js
+++ server/s3_client.js
@@ -4,11 +4,6 @@ var AWS = require('aws-sdk');
 module.exports = function createS3Client() {
   const awsConfig = (process.env.NODE_ENV === 'development')
     ? JSON.parse(fs.readFileSync('./tmp/aws_message_popup.json'))
-    : {
-        accessKeyId: process.env.MESSAGE_POPUP_S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.MESSAGE_POPUP_S3_SECRET_ACCESS_KEY,
-        region: process.env.MESSAGE_POPUP_S3_REGION,
-        bucket: process.env.MESSAGE_POPUP_S3_BUCKET
-      };
+    : JSON.parse(process.env.MESSAGE_POPUP_S3_CONFIG_JSON);
   return new AWS.S3(awsConfig); 
 }
