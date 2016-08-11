2ef82d2e748df7eaad883e6f3ad3362ad24feaa3
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-08-11 13:50:25 -0400

    Functional refactor, but still need to split out Scenario, Prompt, Response and simplify
    Still broken
    
    Mostly done working pass

--- ui/src/message_popup/popup_question.jsx
+++ ui/src/message_popup/popup_question.jsx
@@ -1,7 +1,7 @@
 /* @flow weak */
 import React from 'react';
+import _ from 'lodash';
 import SetIntervalMixin from '../helpers/set_interval_mixin.js';
-import TextChangeEvent from '../types/dom_types.js';
 import StudentCard from './student_card.jsx';
 import HintCard from './renderers/hint_card.jsx';
 import TextField from 'material-ui/TextField';
@@ -11,6 +11,193 @@ import FeedbackCard from './feedback_card.jsx';
 import SummaryCard from './summary_card.jsx';
 import VelocityTransitionGroup from "velocity-react/velocity-transition-group";
 const ONE_SECOND = 1000;
+
+
+const ScenarioRenderer = React.createClass({
+  displayName: 'ScenarioRenderer',
+
+  propTypes: {
+    question: React.PropTypes.object.isRequired
+  },
+
+  render() {
+    const {question} = this.props;
+    return <div style={styles.question}>{question.text}</div>;
+  }
+});
+
+
+const PromptsRenderer = React.createClass({
+  displayName: 'PromptsRenderer',
+
+  propTypes: {
+    scaffolding: React.PropTypes.object.isRequired,
+    question: React.PropTypes.object.isRequired,
+    student: React.PropTypes.object
+  },
+
+  render() {
+    const {scaffolding, student, question} = this.props;
+    const {examples, nonExamples} = question;
+
+    return (
+      <div>
+        {scaffolding.shouldShowStudentCard && student &&
+          <StudentCard useCardStyles={true} student={student} />}
+        {scaffolding.helpType === 'hints' && 
+          <div style={styles.hintCard}>
+            <HintCard examples={examples} nonExamples={nonExamples} />
+          </div>}
+      </div>
+    );
+  }
+});
+
+const ResponseRenderer = React.createClass({
+  displayName: 'ResponseRenderer',
+
+  mixins: [SetIntervalMixin],
+
+  propTypes: {
+    question: React.PropTypes.object.isRequired,
+    scaffolding: React.PropTypes.object.isRequired,
+    limitMs: React.PropTypes.number.isRequired,
+    onLogMessage: React.PropTypes.func.isRequired,
+    onResponseSubmitted: React.PropTypes.func.isRequired
+  },
+
+  getInitialState() {
+    return {
+      isRevising: false,
+      isDoneRevising: false,
+      didRevise: false,
+      initialResponseText: '',
+      finalResponseText: '',
+      elapsedMs: 0
+    };
+  },
+
+  componentDidMount() {
+    this.setInterval(this.onTimerTick, ONE_SECOND);
+  },
+  
+  logMessage(type) {
+    const {initialResponseText, finalResponseText, elapsedMs} = this.state;    
+    this.props.onLogMessage(type, {
+      initialResponseText,
+      finalResponseText,
+      elapsedMs
+    });
+  },
+
+  submitResponse() {
+    const {
+      initialResponseText,
+      finalResponseText,
+      elapsedMs,
+      didRevise
+    } = this.state;
+    this.props.onResponseSubmitted({
+      initialResponseText,
+      finalResponseText,
+      elapsedMs,
+      didRevise
+    });
+  },
+
+  onTimerTick() {
+    if (!this.state.isDoneRevising) {
+      this.setState({ elapsedMs: this.state.elapsedMs + ONE_SECOND });
+    }
+  },
+
+  onTextChanged(e) {
+    const initialResponseText = e.target.value;
+    this.setState({
+      initialResponseText,
+      finalResponseText: initialResponseText
+    });
+  },
+
+  // State transitions:
+  //   initial -> revising
+  //   initial -> done
+  //   revising -> done
+  onRespondClicked() {
+    const {isDoneRevising} = this.state;    
+    const {scaffolding} = this.props;
+    const {examples} = this.props.question;
+
+    // Capture initial response, transition to revising.
+    if (scaffolding.helpType === 'feedback' && examples.length > 0 && !isDoneRevising) {
+      this.logMessage('message_popup_response');
+      this.setState({ isRevising: true });
+      return;
+    }
+  },
+
+  onRevised(finalResponseText) {
+    const {initialResponseText} = this.state;
+    const didRevise = !_.isEqual(finalResponseText, initialResponseText);
+    this.setState({finalResponseText, didRevise}, () => {
+      this.logMessage('message_popup_revision');
+      this.submitResponse();
+    });
+  },
+
+  onPassed() {
+    this.logMessage('message_popup_revision_declined');
+    this.submitResponse();
+  },
+
+  render() {
+    const {scaffolding, limitMs} = this.props;
+    const {examples} = this.props.question;
+    const {isRevising, initialResponseText, elapsedMs} = this.state;
+    const seconds = Math.round(elapsedMs / 1000);
+
+    return (
+      <div>
+        <div style={styles.textAreaContainer}>
+          <TextField
+            style={styles.textField}
+            textareaStyle={styles.textareaInner}
+            underlineShow={false}
+            floatingLabelText='Speak directly to the student'
+            onChange={this.onTextChanged}
+            multiLine={true}
+            disabled={isRevising} 
+            rows={2}/>
+        </div>
+        <div style={styles.buttonRow}>
+          <RaisedButton
+            onTouchTap={this.onRespondClicked}
+            style={styles.button}
+            secondary={true}
+            label={scaffolding.helpType === 'feedback' ? 'Save Response' : 'Respond'}
+            disabled={isRevising || initialResponseText === ''}/>
+          {seconds > 0 && <div style= {styles.ticker}>{seconds}s</div>
+          }
+        </div>
+        <VelocityTransitionGroup enter={{animation: "slideDown"}} runOnMount={true}>
+          {this.state.isRevising &&
+            <div style={styles.feedbackCard}>
+              <FeedbackCard
+                initialResponseText={initialResponseText}
+                onRevised={this.onRevised}
+                onPassed={this.onPassed}
+                examples={examples}/>  
+            </div>}
+        </VelocityTransitionGroup>
+        <Snackbar
+          open={seconds >= (limitMs/1000)}
+          message="Remember, these are supposed to be quick responses."
+          onRequestClose={this.onRequestClose}
+          />
+      </div>
+    );
+  }
+});
 
 /*
 Shows a single question.
@@ -26,7 +213,6 @@ export type Response = {
 };
 export default React.createClass({
   displayName: 'PopupQuestion',
-  mixins: [SetIntervalMixin],
 
   propTypes: {
     scaffolding: React.PropTypes.shape({
@@ -49,40 +235,13 @@ export default React.createClass({
 
   getInitialState: function() {
     return {
-      elapsedMs: 0,
-      initialResponseText: '',
-      finalResponseText: undefined,
-      isRevising: false,
-      isDoneRevising: false
+      response: null
     };
-  },
-
-  componentDidMount() {
-    this.setInterval(this.updateTimer, ONE_SECOND);
-  },
-
-  updateTimer() {
-    if(!this.state.isDoneRevising){
-      this.setState({ elapsedMs: this.state.elapsedMs + ONE_SECOND });
-    }
-  },
-
-  logResponse() {
-    this.logData('message_popup_response');
-  },
-  
-  logRevision(finalText) {
-    this.logData('message_popup_revision', {finalResponseText: finalText});
-  },
-  
-  logRevisionDeclined(){
-    this.logData('message_popup_revision_declined');
   },
   
   logData(type, params = {}) {
-    const {elapsedMs, initialResponseText} = this.state;
     const {question, scaffolding} = this.props;
-    const {finalResponseText} = params;
+    const {finalResponseText, initialResponseText, elapsedMs} = params;
     const response:Response = {
       question,
       shouldShowStudentCard: scaffolding.shouldShowStudentCard,
@@ -94,135 +253,77 @@ export default React.createClass({
     this.props.onLog(type, response);
   },
   
-  onTextChanged({target:{value}}:TextChangeEvent) {
-    this.setState({ initialResponseText: value });
-  },
-  
-  onDonePressed(text=undefined) {
-    if(!this.props.scaffolding.shouldShowSummary){
-      this.props.onDone(this.state.elapsedMs);
-      return;
-    }
-    
-    if(this.state.isDoneRevising){
-      this.props.onDone(this.state.elapsedMs);
-      return;
-    }
-    
-    if(!this.state.isDoneRevising){
-      var finalText = (text === undefined ? this.state.initialResponseText : text);
-      this.setState({finalResponseText: finalText});
-      this.setState({isDoneRevising: true});
-      return;
-    }
-  },
-
-  onSavePressed() {
-    this.logResponse();
-    const {scaffolding} = this.props;
-    const {examples} = this.props.question;
-    if(scaffolding.helpType === 'feedback' && examples.length > 0) {
-      this.setState({ isRevising: true });
-    } else{
-      this.onDonePressed();
-    }
-  },
-  
-  onRevised(text) {
-    this.logRevision(text);
-    this.onDonePressed(text);
-  },
-  onPassed() {
-    this.logRevisionDeclined();
-    this.onDonePressed();
-  },
-
   onRequestClose() {
     //This empty function is meant to cancel out the closing of the toast without having to use
     //an extremely large autoHideDuration
   },
 
+  onResponseSubmitted(response) {
+    console.log('onResponseSubmitted', response);
+    this.setState({response});
+  },
+
+  onDone(finalResponseText) {
+    console.log('finalResponseText', finalResponseText);
+    this.props.onDone(this.state.response.elapsedMs * 1000);
+  },
+
   render() {
-    const {isDoneRevising} = this.state;
+    const {response} = this.state;
     const {scaffolding} = this.props;  
+
+    const shouldRenderQuestion = (
+      (!scaffolding.shouldShowSummary) ||
+      (!response && scaffolding.shouldShowSummary)
+    );
+
     return (
       <div>
         <VelocityTransitionGroup leave={{animation: "slideUp"}} runOnMount={true}>
-          {!scaffolding.shouldShowSummary && this.renderQuestion()}
-          {!isDoneRevising && scaffolding.shouldShowSummary && this.renderQuestion()}
+          {shouldRenderQuestion && this.renderQuestion()}
         </VelocityTransitionGroup>
         <VelocityTransitionGroup enter={{animation: "slideDown"}} runOnMount={true}>
-          {isDoneRevising && scaffolding.shouldShowSummary && this.renderSummary()}
+          {response && scaffolding.shouldShowSummary && this.renderSummary()}
         </VelocityTransitionGroup>
       </div>
     );
   },
   
   renderQuestion(){
-    const {elapsedMs} = this.state;
-    const {scaffolding} = this.props;
-    const {text, students, examples, nonExamples} = this.props.question;
-    const seconds = Math.round(elapsedMs / 1000);
-    const student = students !== undefined ? students[0] : undefined;
+    const {scaffolding, limitMs, question} = this.props;
+    const student = _.first(question.students || []);
     
     return (
       <div>
-        <div style={styles.question}>{text}</div>
-        {scaffolding.shouldShowStudentCard && student &&
-          <StudentCard useCardStyles={true} student={student} />}
-        {scaffolding.helpType === 'hints' && 
-          <div style={styles.hintCard}>
-            <HintCard examples={examples} nonExamples={nonExamples} />
-          </div>}
-        <div style={styles.textAreaContainer}>
-          <TextField
-            style={styles.textField}
-            textareaStyle={styles.textareaInner}
-            underlineShow={false}
-            floatingLabelText='Speak directly to the student'
-            onChange={this.onTextChanged}
-            multiLine={true}
-            disabled={this.state.isRevising} 
-            rows={2}/>
-        </div>
-        <div style={styles.buttonRow}>
-          <RaisedButton
-            onTouchTap={this.onSavePressed}
-            style={styles.button}
-            secondary={true}
-            label={scaffolding.helpType === 'feedback' ? 'Save Response' : 'Respond'}
-            disabled={this.state.isRevising || this.state.initialResponseText === ''}/>
-          {seconds > 0 &&
-            <div style={styles.ticker}>{seconds}s</div>
-          }
-        </div>
-        <VelocityTransitionGroup enter={{animation: "slideDown"}} runOnMount={true}>
-          {this.state.isRevising &&
-            <div style={styles.feedbackCard}>
-              <FeedbackCard
-                initialResponseText={this.state.initialResponseText}
-                onRevised={this.onRevised}
-                onPassed={this.onPassed}
-                examples={examples}/>  
-            </div>}
-        </VelocityTransitionGroup>
-        <Snackbar
-          open={seconds >= (this.props.limitMs/1000)}
-          message="Remember, these are supposed to be quick responses."
-          onRequestClose={this.onRequestClose}
-          />
+        <ScenarioRenderer
+          question={this.props.question} />
+        <PromptsRenderer
+          scaffolding={scaffolding}
+          student={student}
+          question={this.props.question}
+        />
+        <ResponseRenderer
+          scaffolding={scaffolding}
+          question={question}
+          limitMs={limitMs}
+          onResponseSubmitted={this.onResponseSubmitted}
+          onLogMessage={this.logData}
+        />
       </div>
     );
   },
   
-  renderSummary(){
+  renderSummary() {
+    const {elapsedMs, finalResponseText, initialResponseText} = this.state.response;
+    const {question, scaffolding, isLastQuestion} = this.props;
+
     return (
       <SummaryCard 
-        onDone={this.onDonePressed} 
-        question={this.props.question} 
-        response={this.props.scaffolding.helpType==='feedback' ? this.state.finalResponseText : this.state.initialResponseText} 
-        elapsedSeconds={Math.round(this.state.elapsedMs / 1000)} 
-        buttonLabel={this.props.isLastQuestion ? "Finish" : "Next Question"}/>
+        onDone={this.onDone} 
+        question={question} 
+        response={scaffolding.helpType ==='feedback' ? finalResponseText : initialResponseText} 
+        elapsedSeconds={Math.round(elapsedMs / 1000)} 
+        buttonLabel={isLastQuestion ? "Finish" : "Next Question"}/>
     );
     
   }
