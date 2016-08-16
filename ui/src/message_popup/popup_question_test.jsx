9f93ea051a858622a9623389aa27be814041ae87
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-08-16 17:03:37 -0400

    Fixup test for PopupQuestion

--- ui/src/message_popup/popup_question_test.jsx
+++ ui/src/message_popup/popup_question_test.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import sinon from 'sinon';
 import {expect} from 'chai';
 import {allQuestions} from './questions.js';
 import {withStudents} from './transformations.jsx';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import PopupQuestion from './popup_question.jsx';
 import SummaryCard from './summary_card.jsx';
@@ -14,35 +13,28 @@ import ScenarioRenderer from './renderers/scenario_renderer.jsx';
 import PromptsRenderer from './renderers/prompts_renderer.jsx';
 import ResponseRenderer from './renderers/response_renderer.jsx';
 
-const testQuestion = _.first(withStudents(allQuestions));
-const practiceScaffolding = {
-  helpType: 'feedback',
-  shouldShowStudentCard: true,
-  shouldShowSummary: true
+const helpers = {
+  testQuestion: _.first(withStudents(allQuestions)),
+  practiceScaffolding: {
+    helpType: 'feedback',
+    shouldShowStudentCard: true,
+    shouldShowSummary: true
+  },
+  solutionScaffolding: {
+    helpType: 'solution',
+    shouldShowStudentCard: true,
+    shouldShowSummary: false
+  }
 };
 
-const solutionScaffolding = {
-  helpType: 'solution',
-  shouldShowStudentCard: true,
-  shouldShowSummary: false
-};
-
-function createResponse() {
-  return {
-    initialResponseText: 'foo',
-    finalResponseText: 'bar',
-    elapsedMs: 4303,
-    didRevise: false
-  };
-}
 
 function testProps(props) {
   const onLogSpy = sinon.spy();
   const onDoneSpy = sinon.spy();
 
   return {
-    question: testQuestion,
-    scaffolding: practiceScaffolding,
+    question: helpers.testQuestion,
+    scaffolding: helpers.practiceScaffolding,
     limitMs: 30000,
     onLog: onLogSpy,
     onDone: onDoneSpy,
@@ -63,7 +55,7 @@ describe('<PopupQuestion />', () => {
   });
 
   it('renders the question in solution mode', () => {    
-    const props = testProps({ scaffolding: solutionScaffolding });
+    const props = testProps({ scaffolding: helpers.solutionScaffolding });
     const wrapper = shallow(<PopupQuestion {...props} />);
     expect(wrapper.find(ScenarioRenderer).length).to.equal(1);
     expect(wrapper.find(PromptsRenderer).length).to.equal(1);
