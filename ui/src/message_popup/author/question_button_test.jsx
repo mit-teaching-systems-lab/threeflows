cfede90943a5cb027e567e1ba8060b6f91b6adc0
David Rosales <david.anekin.rosales@gmail.com>
2016-08-17 13:12:14 -0400

    Added more QuestionButton tests

--- ui/src/message_popup/author/question_button_test.jsx
+++ ui/src/message_popup/author/question_button_test.jsx
@@ -3,15 +3,19 @@ import {mount, shallow, render} from 'enzyme';
 import {expect} from 'chai';
 
 import QuestionButton from './question_button.jsx';
-import RaisedButton from 'material-ui/RaisedButton';
+import {testQuestion} from '../test_fixtures.js';
+import {ListItem} from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 describe('<QuestionButton />', () => {
-  it('can shallow render without a Material UI Theme', () => {
-    const wrapper = shallow(<Foo hello="world" />);
-    expect(wrapper.find(RaisedButton)).to.have.length(1);
-    expect(wrapper.contains(<RaisedButton label="success!" />)).to.equal(true);
-    expect(wrapper.find('.plain-div')).to.have.length(1);
-    expect(wrapper.find('.plain-div').text()).to.equal('hello');
+  it('can shallow render with question text', () => {
+    const wrapper = shallow(<QuestionButton question={testQuestion}/>);
+    expect(wrapper.find(ListItem).props().secondaryText).to.equal(testQuestion.text);
+  });
+
+  it('can navigate when touched', () => {
+    const wrapper = shallow(<QuestionButton question={testQuestion}/>);
+    wrapper.find(ListItem).simulate('touchTap');
+    expect(window.location.href).to.equal(`/teachermoments/author/questions/${testQuestion.id}`);
   });
 });
