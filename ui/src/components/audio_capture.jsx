2bfc658122d536435fe736560bc5364130b88471
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-08-29 11:47:37 -0400

    Replacing AuO and addressing dropped audio frames

--- ui/src/components/audio_capture.jsx
+++ ui/src/components/audio_capture.jsx
@@ -1,90 +1,10 @@
 /* @flow weak */
 import React from 'react';
-import {AuO} from './AuO.js';
+import AudioRecorder from './audio_recorder.js';
 
 
 /*
-This wraps Au0 and uses it to grab audio on Chrome and Firefox for a
-one-time use recording.
-We want direct access to the blob and don't want the UI, but since the UI
-is rendering the audio stream, the microphone stays active.
-*/
-class WilliamRecorder {
-  auo: ?Object;
-  inlineStyleNode: ?HTMLStyleElement;
-  onBlobReady: ?Function;
-
-  _error(...params) {
-    console.error(params); // eslint-disable-line no-console
-  }
-
-  // Hide the UI
-  _injectStyles() {
-    const inlineStyleNode = this.inlineStyleNode = document.createElement('style');
-    inlineStyleNode.type = 'text/css';
-    const styleText = document.createTextNode(`
-      .AuO * {
-        display: none;
-        color: red;
-      }
-    `);
-    inlineStyleNode.appendChild(styleText);
-    document.head.appendChild(inlineStyleNode);
-  }
-
-  record() {
-    this._injectStyles();
-    const auo = new AuO(null, null, this._onSaveWhenOffline.bind(this));
-    auo.launch();
-    auo.record();
-    this.auo = auo;
-  }
-
-  stop(onBlobReady) {
-    const auo = this.auo;
-    if (!auo) return this._error('Unexpected state: auo', auo);
-    if (this.onBlobReady) return this._error('Unexpected state: this.onBlobReady', this.onBlobReady);
-
-    this.onBlobReady = onBlobReady;
-    auo.stop(this._onAudioProcessed.bind(this));
-  }
-
-  // Force offline mode to just get the blob ourselves
-  _onAudioProcessed() {
-    const auo = this.auo;
-    if (!auo) return this._error('Unexpected state: auo', auo);
-
-    auo.setOnline(false);
-    auo.save();
-  }
-  
-  _onSaveWhenOffline(blob) {
-    if (!this.onBlobReady) return this._error('Unexpected state: this.onBlobReady', this.onBlobReady);
-    this.onBlobReady(blob);
-    this.destroy();
-  }
-
-  destroy() {
-    if (this.auo) {
-      this.auo.close();
-      delete this.auo;
-    }
-    if (this.inlineStyleNode) {
-      document.head.removeChild(this.inlineStyleNode);
-      delete this.inlineStyleNode;
-    }
-  }
-}
-
-
-/*
-This is a React API to capture audio using a fork of the AuO widget.
-
-It's a hack and hides all the UI features of AuO, while taking
-advantage of its audio capture and encoding features.
-
-Future work could swap in another implementation here, or extract the
-necessary code from AuO.
+This is a React API to capture audio using an audio recorder class.
 */
 export default React.createClass({
   displayName: 'AudioCapture',
@@ -117,7 +37,7 @@ export default React.createClass({
   recorder:(null: ?Object),
   
   startRecording() {
-    const recorder = this.recorder = new WilliamRecorder();
+    const recorder = this.recorder = new AudioRecorder();
     recorder.record();
   },
 
