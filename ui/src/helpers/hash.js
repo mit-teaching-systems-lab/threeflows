729a83adf2d738b9f9c335661a777da852705a9b
Kevin Ng <keving17@mit.edu>
2018-02-08 13:46:02 -0500

    removes old ui folder

--- ui/src/helpers/hash.js
+++
@@ -1,7 +0,0 @@
-// Hash a string to a 32-bit integer
-export default function hash(str) {
-  return str.split('').reduce((num, c) => {
-    const updated = ((num<<5) - num) + c.charCodeAt(0);
-    return updated & updated;
-  }, 0);
-}
