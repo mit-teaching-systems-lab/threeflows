f57169b767ebf7ac51c1151bbf2b2bf2c19bdb87
Kevin Ng <keving17@mit.edu>
2018-03-28 22:27:21 -0400

    first iteration of grabbing consented data

--- server/database.js
+++ server/database.js
@@ -5,9 +5,40 @@ const {getDomain} = require('./domain.js');
 //of users and returns all rows in the evidence table with that url
 //Returns 200 unless there was an error with the database query
 function dataEndpoint(pool, request, response) {
+  const token = request.headers['x-teachermoments-token'];
   const domain = getDomain(request);
   const location = `${domain}/teachermoments/turner?KevinTesting20180319`;
 
+  checkAccess(pool, location, token)
+    .then(isTokenAuthorized => {
+      if (isTokenAuthorized) {
+        getData(pool, location)
+          .then(results => {
+            response.set('Content-Type', 'application/json');
+            response.json({
+              evidence: {rows: results} 
+            });
+            return response.status(200).end()
+          });
+      }
+      else{
+        console.log('Researcher is not authorized to see this data')
+        return response.status(405).end();
+      }
+    })
+    .catch(err => {
+      console.log('Error with database query:', err);
+      return response.status(500).end();
+    });
+}
+
+// Spoofing the part where I confirm a token is allowed to view data from 
+// a specific location. Location is currently hard coded so this is fine.
+function checkAccess(pool, location, token) {
+  return Promise.resolve(true);
+}
+
+function getData(pool, location){
   const values = [location];
   const sql = `
     SELECT *
@@ -16,23 +47,10 @@ function dataEndpoint(pool, request, response) {
       AND json->'GLOBAL'->>'location' = $1
     ORDER BY json->>'sessionId', id ASC;`;
 
-  pool.query(sql,values)
-    .then(results => {
-      if (results.rowCount >= 1){
-        response.set('Content-Type', 'application/json');
-        response.json({
-          evidence: {rows: results.rows} 
-        });
-        return response.status(200).end();
-      }
-      else{
-        console.log('Could not find data with url:', location);
-        return response.status(200).end();
-      }
-    })
+  return pool.query(sql,values)
+    .then(results => results.rows)
     .catch(err => {
       console.log('Error with database query:', err);
-      return response.status(500).end();
     });
 }
 
